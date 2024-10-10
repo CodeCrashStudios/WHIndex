be01d71f13c2a990c10be360036f7a782d6912d9
CCStudios <50129568+CodeCrashStudios@users.noreply.github.com>
2024-10-09 23:12:46 -0400

    Made the map more readable.

--- main.js
+++ main.js
@@ -5,6 +5,23 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+var greenFeatheredIcon = L.icon({
+    iconUrl: 'GreenMark.png',
+
+    iconSize:     [32, 32], // size of the icon
+    iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
+});
+
+var redFeatheredIcon = L.icon({
+    iconUrl: 'RedMark.png',
+
+    iconSize:     [32, 32], // size of the icon
+    iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
+});
+
+map.createPane('closed');
+map.getPane('closed').style.zIndex = 999;
+
 fetchDataToPoints();
 
 async function extractJSONData(url) {
@@ -38,7 +55,8 @@ async function fetchDataToPoints() {
     for (let i = 0; i < locations.length; i++) {
         let isClosed = (locations[i].specialHours != null);
 
-        L.circleMarker([locations[i].latitude, locations[i].longitude], { radius: 1, color: isClosed ? '#FF0000' : '#00FF00' }).addTo(map);
+        if (isClosed)L.marker([locations[i].latitude, locations[i].longitude], { icon: redFeatheredIcon, pane: 'closed'}).addTo(map);
+        else L.marker([locations[i].latitude, locations[i].longitude], { icon: greenFeatheredIcon }).addTo(map);
 
     }
 
